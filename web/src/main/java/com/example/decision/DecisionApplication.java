460d7859baff27301c7e625ed691405895df2735
Shuichi Kudo <swings.chalky_0l@icloud.com>
2026-02-24 13:15:36 +0900

    環境構築

---
+++ web/src/main/java/com/example/decision/DecisionApplication.java
@@ -0,0 +1,11 @@
+package com.example.decision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecisionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DecisionApplication.class, args);
+    }
+}
